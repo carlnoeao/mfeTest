fba0a3f639953b3b41651f203f9a4caf4b818792
Noé <ncarl@alphaomega.lu>
2023-05-11 11:43:20 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("TEMPLATE"));
+const root = ReactDOM.createRoot(document.getElementById('TEMPLATE'));
 root.render(
   <React.StrictMode>
     <App />
+    {test}
   </React.StrictMode>
 );
+
+
+const test = <p>test</p>;
