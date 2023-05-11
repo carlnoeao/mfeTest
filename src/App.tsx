871997fb5d5f00cff0177b5708ff73926f7bb4e5
Noé <ncarl@alphaomega.lu>
2023-05-11 14:37:22 +0200

    a few fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Example from '@ao/shared/components/Example';
+import Button from '@mui/material/Button';
+
 function App() {
-  return <div className='App'>I'm a basic MFE</div>;
+  return (
+    <div className='App'>
+      I'm a basic MFE
+      <Example />
+      <Button variant='contained'>Hello World</Button>
+    </div>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,8 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const baseConfig = require('../../webpack.config.base');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            ['@babel/preset-react', { runtime: 'automatic' }],
-                            '@babel/preset-typescript',
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    resolve: {
-        alias: {
-            'shared': path.resolve(__dirname, '..', 'shared'),
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 3001,
-        hot: true,
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            inject: 'body',
-        }),
-    ],
-};
+module.exports = baseConfig({
+    entryPoint: './src/index.tsx',
+    outputPath: `${__dirname}/dist`,
+    templatePath: './public/index.html',
+    port: 3000, // choose a unique port for each MFE
+});
